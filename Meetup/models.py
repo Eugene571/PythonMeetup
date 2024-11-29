d16c9429aafd3c476d805a51b92972fe1c570fa1
RaudSam <feltson666@mail.ru>
2024-11-29 17:44:33 +0300

    Внесла модели

--- Meetup/models.py
+++ Meetup/models.py
@@ -1,14 +1,48 @@
 from django.db import models
 from django.utils.timezone import now
 
+
+class Users(models.Model):
+    telegram_id = models.IntegerField(
+        verbose_name="телеграм id",
+        max_length=50,
+        blank=True,
+        unique=True,
+        db_index=True
+        )
+    tg_nick = models.CharField(
+        verbose_name='Ник в телеграм',
+        max_length=50,
+        blank=True
+    )
+    username = models.CharField(
+        verbose_name='Имя',
+        max_length=100,
+        null=True,
+        blank=False
+        )
+
+    def __str__(self) -> str:
+        return f"{self.name} - {self.tg_id}"
+
+
 class Speaker(models.Model):
     """Модель для представления докладчика."""
+    telegram_id = models.ForeignKey(
+        Users,
+        on_delete=models.DO_NOTHING,
+        related_name='tg_id',
+        null=True,
+        blank=True,
+        default=False
+        )
     name = models.CharField(max_length=255, verbose_name="Имя докладчика")
     bio = models.TextField(blank=True, verbose_name="Биография")
     contact = models.EmailField(blank=True, verbose_name="Контактный email")
 
     def __str__(self):
         return self.name
+
 
 class Question(models.Model):
     """Модель для представления вопросов к докладчикам."""
@@ -21,6 +55,7 @@ class Question(models.Model):
     def __str__(self):
         return f"Вопрос от {self.user_name} к {self.speaker}"
 
+
 class Event(models.Model):
     """Модель для представления мероприятия."""
     title = models.CharField(max_length=255, verbose_name="Название мероприятия")
@@ -31,5 +66,3 @@ class Event(models.Model):
 
     def __str__(self):
         return self.title
-
-
