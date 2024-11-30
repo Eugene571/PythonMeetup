91655a1dd419dddc10b5ce439ddf08b9a8dde0c6
Eugene Pisarev <zm1s3ry@gmail.com>
2024-11-30 13:23:30 +0300

    - Исправлен requirements.txt (были указаны библиотеки сопутствующие к основным) - Исправлены импорты в боте, версия стабильная и работает - Исправлена модель user(s) - прибавлялась дополнительная 's' в админке Django

--- Meetup/admin.py
+++ Meetup/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Question, Speaker, Event, Users
+from .models import Question, Speaker, Event, User
 
 admin.site.register(Question)
 admin.site.register(Speaker)
 admin.site.register(Event)
-admin.site.register(Users)
+admin.site.register(User)
 
--- Meetup/models.py
+++ Meetup/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils.timezone import now
 
 
-class Users(models.Model):
+class User(models.Model):
     telegram_id = models.IntegerField(
         verbose_name="телеграм id",
         max_length=50,
@@ -29,16 +29,25 @@ class Users(models.Model):
 class Speaker(models.Model):
     """Модель для представления докладчика."""
     telegram_id = models.ForeignKey(
-        Users,
+        User,
         on_delete=models.DO_NOTHING,
         related_name='tg_id',
         null=True,
         blank=True,
         default=False
         )
-    name = models.CharField(max_length=255, verbose_name="Имя докладчика")
-    bio = models.TextField(blank=True, verbose_name="Биография")
-    contact = models.EmailField(blank=True, verbose_name="Контактный email")
+    name = models.CharField(
+        max_length=255,
+        verbose_name="Имя докладчика"
+        )
+    bio = models.TextField(
+        blank=True,
+        verbose_name="Биография"
+        )
+    contact = models.EmailField(
+        blank=True,
+        verbose_name="Контактный email"
+        )
 
     def __str__(self):
         return self.name
@@ -46,11 +55,27 @@ class Speaker(models.Model):
 
 class Question(models.Model):
     """Модель для представления вопросов к докладчикам."""
-    speaker = models.ForeignKey(Speaker, on_delete=models.CASCADE, related_name="questions", verbose_name="Докладчик")
-    text = models.TextField(verbose_name="Текст вопроса")
-    user_name = models.CharField(max_length=255, verbose_name="Имя пользователя, задавшего вопрос")
-    created_at = models.DateTimeField(default=now, verbose_name="Дата и время создания")
-    is_answered = models.BooleanField(default=False, verbose_name="Ответ дан")
+    speaker = models.ForeignKey(
+        Speaker,
+        on_delete=models.CASCADE,
+        related_name="questions",
+        verbose_name="Докладчик"
+    )
+    text = models.TextField(
+        verbose_name="Текст вопроса"
+    )
+    user_name = models.CharField(
+        max_length=255,
+        verbose_name="Имя пользователя, задавшего вопрос"
+    )
+    created_at = models.DateTimeField(
+        default=now,
+        verbose_name="Дата и время создания"
+    )
+    is_answered = models.BooleanField(
+        default=False,
+        verbose_name="Ответ дан"
+    )
 
     def __str__(self):
         return f"Вопрос от {self.user_name} к {self.speaker}"
@@ -58,11 +83,25 @@ class Question(models.Model):
 
 class Event(models.Model):
     """Модель для представления мероприятия."""
-    title = models.CharField(max_length=255, verbose_name="Название мероприятия")
-    description = models.TextField(blank=True, verbose_name="Описание мероприятия")
-    start_time = models.DateTimeField(verbose_name="Дата и время начала")
-    end_time = models.DateTimeField(verbose_name="Дата и время окончания")
-    speakers = models.ManyToManyField(Speaker, related_name="events", verbose_name="Докладчики")
+    title = models.CharField(
+        max_length=255,
+        verbose_name="Название мероприятия"
+    )
+    description = models.TextField(
+        blank=True,
+        verbose_name="Описание мероприятия"
+    )
+    start_time = models.DateTimeField(
+        verbose_name="Дата и время начала"
+    )
+    end_time = models.DateTimeField(
+        verbose_name="Дата и время окончания"
+    )
+    speakers = models.ManyToManyField(
+        Speaker,
+        related_name="events",
+        verbose_name="Докладчики"
+    )
 
     def __str__(self):
         return self.title
